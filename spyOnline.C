096145704fcd58dfacd7661fbec820909765fd51
Nicolas de Sereville <deserevi@ipno.in2p3.fr>
2013-12-06 11:13:13 +0100

    + exp name is now taken from the USERNAME env. variable

--- spyOnline.C
+++ spyOnline.C
@@ -1,24 +1,31 @@
 {  
    gROOT->Reset(); 
 
+   // load GUser
    TString GRUpath = gSystem->Getenv("GRUDIR");
    gROOT->ProcessLine(Form(".include %s/include", GRUpath.Data()));
    gROOT->ProcessLine(".L ./GUser_C.so");   //load and compile GUser class 
 
+   // connect to data flow
 //   GNetClientNarval *net = new GNetClientNarval("193.48.111.163"); //
    GNetClientNarval *net = new GNetClientNarval("localhost"); //
    net->SetPort(10202);  
    net->SetBufferSize(65536);
 
-   GUser *a = new GUser(net);  // create user treatement environement 
+   // instantiate GUSer object
+   GUser *a = new GUser(net);                      // create user treatement environement 
+   // instantiate spectrum server for vigru
    GNetServerRoot *serv = new GNetServerRoot(9090, a);
 
-   a->EventInit("e644");       // event initialisation 
-   a->SetSpectraMode(1);       // Declare all raw parameters as histograms 
-   a->SetUserMode(1);	         // execute GUser::InitUser()
+   // get exp. name
+   TString ExpName = gSystem->Getenv("USERNAME");
+   a->EventInit(ExpName.Data());                   // event initialisation 
+   a->SetSpectraMode(1);                           // Declare all raw parameters as histograms 
+   a->SetUserMode(1);	                           // execute GUser::InitUser()
 
+   // start hist server
    serv->StartServer();
-   a->DoRun();                 // a->DoRun(2000); do treaments on 2000 first events ( 0 = all);
+   a->DoRun();                                     // a->DoRun(2000); do treaments on 2000 first events ( 0 = all);
 
    net->Close();                       
 
